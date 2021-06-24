94af4b3136f55254a2eeba4a5863c9d8b2c8ec28
self.programmed <self.programmed@gmail.com>
2021-06-24 15:43:49 +0530

    changed removed build folder again

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.use(planetsRouter)
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', /*  */ 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
 
 module.exports = app
