2ad7bb730b0b334b915ff7b886a8e3e2c53e68e3
Subham Ray <self.programmed@gmail.com>
2021-07-03 14:36:17 +0530

    added env

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,13 @@
 const http = require('http')
+const mongoose = require('mongoose')
 
 const app = require('./app')
 
 const { loadAllPlanets } = require('./models/planets.model')
 
 const PORT = process.env.PORT || 5000
+
+
 
 const server = http.createServer(app)
 
