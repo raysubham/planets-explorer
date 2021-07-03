a229d2528bfed1e9a9b99e00c150e5618454102e
Subham Ray <self.programmed@gmail.com>
2021-07-03 15:01:48 +0530

    env add

--- server/src/server.js
+++ server/src/server.js
@@ -7,11 +7,25 @@ const { loadAllPlanets } = require('./models/planets.model')
 
 const PORT = process.env.PORT || 5000
 
-
+const MONGO_URL =
+  'mongodb+srv://planets-api:bppQGlNsKdfh5Gi3@planets-database.buupv.mongodb.net/planets?retryWrites=true&w=majority'
 
 const server = http.createServer(app)
 
+mongoose.connection.once('open', () => {
+  console.log(`MongoDB started!`)
+})
+mongoose.connection.on('error', (err) => {
+  console.error(err)
+})
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
   await loadAllPlanets()
 
   server.listen(PORT, () => {
