26ed8ff40936640fc923aafb615c8bf4f6a71c52
Subham Ray <self.programmed@gmail.com>
2021-07-14 23:08:48 +0530

    docker-ized the web app

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,14 +1,23 @@
-import { Footer as ArwesFooter, Paragraph } from "arwes";
-import Centered from "./Centered";
+import { Footer as ArwesFooter, Paragraph } from 'arwes'
+import Centered from './Centered'
 
 const Footer = () => {
-  return <ArwesFooter animate>
-    <Centered>
-      <Paragraph style={{ fontSize: 14, margin: "10px 0" }}>
-        This is not an official site and is not affiliated with NASA or SpaceX in any way. For educational purposes only.
-      </Paragraph>
-    </Centered>
-  </ArwesFooter>
-};
+  return (
+    <ArwesFooter animate>
+      <Centered>
+        <Paragraph
+          style={{
+            fontSize: 14,
+            alignItems: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          Made by Subham Ray
+        </Paragraph>
+      </Centered>
+    </ArwesFooter>
+  )
+}
 
-export default Footer;
+export default Footer
